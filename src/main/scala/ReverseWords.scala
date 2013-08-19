36d5d3cf1bb330c4d53241257024f973ee1d34a0
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-08-19 20:30:14 +0100

    Change trait name to avoid any collisions

--- src/main/scala/ReverseWords.scala
+++ src/main/scala/ReverseWords.scala
@@ -3,12 +3,12 @@
  */
 package com.fayi.scripts
 
-trait Solution {
+trait RW {
   def solve(data: List[String]) =
     data.map(l => data.indexWhere(_==l)+1 -> l).map(t => "Case #"+t._1+": "+t._2) foreach println
 }
 
-object ReverseWords extends App with Solution {
+object ReverseWords extends App with RW {
   // val in = io.Source.fromURL(getClass.getResource("/reversewords.in"))
   val in = io.Source.fromURL(getClass.getResource("/reversewords_test.in"))
   val data = in.getLines.drop(1).map(_.split("\\W").toList.reverse.mkString(" ")).toList
