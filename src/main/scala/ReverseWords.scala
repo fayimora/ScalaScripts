34f5cdfbcb0d71fd30f59acbe2f37269e183cb17
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-10-31 13:50:02 +0000

    Streamline Reverse words

--- src/main/scala/ReverseWords.scala
+++ src/main/scala/ReverseWords.scala
@@ -1,14 +1,8 @@
 /*
  * https://code.google.com/codejam/contest/351101/dashboard#s=p1
  */
-trait RW {
-  def solve(data: List[String]) =
-    data.map(l => data.indexWhere(_==l)+1 -> l).map(t => "Case #"+t._1+": "+t._2) foreach println
-}
-
-object ReverseWords extends App with RW {
-  // val in = io.Source.fromURL(getClass.getResource("/reversewords.in"))
+object ReverseWords extends App {
   val in = io.Source.fromURL(getClass.getResource("/reversewords_test.in"))
   val data = in.getLines.drop(1).map(_.split("\\W").toList.reverse.mkString(" ")).toList
-  solve(data)
+  data.map(l => data.indexWhere(_==l)+1 -> l).map(t => "Case #"+t._1+": "+t._2) foreach println
 }
